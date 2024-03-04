a9db1daf3ff06373c519c298e13fa3fcec399f0e
BelovAA <BelovAA@206D-4>
2024-03-04 12:01:22 +0700

    Добавьте файлы проекта.

---
+++ UserControlCreation/ClearableTextBox.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace UserControlCreation
+{
+    partial class ClearableTextBox
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(3, 5);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(35, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "label1";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValue.Location = new System.Drawing.Point(3, 23);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(148, 20);
+            this.txtValue.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(157, 23);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(31, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "↻";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ClearableTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.lblTitle);
+            this.MinimumSize = new System.Drawing.Size(84, 53);
+            this.Name = "ClearableTextBox";
+            this.Size = new System.Drawing.Size(191, 53);
+            this.Load += new System.EventHandler(this.ClearableTextBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ UserControlCreation/ClearableTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlCreation
+{
+    // https://learn.microsoft.com/ru-ru/dotnet/desktop/winforms/controls-design/how-to-create-usercontrol?view=netdesktop-7.0&authuser=0
+    public partial class ClearableTextBox : UserControl
+    {
+        public new string Text
+        {
+            get => txtValue.Text;
+            set => txtValue.Text = value;
+        }
+
+        public string Title
+        {
+            get => lblTitle.Text;
+            set => lblTitle.Text = value;
+        }
+
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+            => Text = "";
+        private void ClearableTextBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
