80a9cb663f59c455128400ab64abc89c460564a1
BelovAA <BelovAA@206D-4>
2024-03-04 12:14:52 +0700

    Сделал изменения

--- UserControlCreation/Form1.Designer.cs
+++ UserControlCreation/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clearableTextBox1 = new UserControlCreation.ClearableTextBox();
+            this.clearableTextBox2 = new UserControlCreation.ClearableTextBox();
             this.SuspendLayout();
+            // 
+            // clearableTextBox1
+            // 
+            this.clearableTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.clearableTextBox1.MinimumSize = new System.Drawing.Size(84, 53);
+            this.clearableTextBox1.Name = "clearableTextBox1";
+            this.clearableTextBox1.Size = new System.Drawing.Size(191, 53);
+            this.clearableTextBox1.TabIndex = 0;
+            this.clearableTextBox1.Title = "label1";
+            // 
+            // clearableTextBox2
+            // 
+            this.clearableTextBox2.Location = new System.Drawing.Point(13, 100);
+            this.clearableTextBox2.MinimumSize = new System.Drawing.Size(84, 53);
+            this.clearableTextBox2.Name = "clearableTextBox2";
+            this.clearableTextBox2.Size = new System.Drawing.Size(191, 53);
+            this.clearableTextBox2.TabIndex = 1;
+            this.clearableTextBox2.Title = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 276);
+            this.Controls.Add(this.clearableTextBox2);
+            this.Controls.Add(this.clearableTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +65,9 @@
         }
 
         #endregion
+
+        private ClearableTextBox clearableTextBox1;
+        private ClearableTextBox clearableTextBox2;
     }
 }
 
