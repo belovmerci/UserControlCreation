3750d797591f486e106f68e989a990e351a66ca7
BelovAA <BelovAA@206D-4>
2024-03-04 12:14:28 +0700

    Made changes

--- UserControlCreation/Form1.cs
+++ UserControlCreation/Form1.cs
@@ -16,5 +16,25 @@ namespace UserControlCreation
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e) { }
+        /*
+        private void Form1_Load(object sender, EventArgs e) =>
+            UpdateNameLabel();
+
+        private void ctlFirstName_TextChanged(object sender, EventArgs e) =>
+            UpdateNameLabel();
+
+        private void ctlLastName_TextChanged(object sender, EventArgs e) =>
+            UpdateNameLabel();
+
+
+        private void UpdateNameLabel()
+        {
+            if (string.IsNullOrWhiteSpace(ctlFirstName.Text) || string.IsNullOrWhiteSpace(ctlLastName.Text))
+                lblFullName.Text = "Please fill out both the first name and the last name.";
+            else
+                lblFullName.Text = $"Hello {ctlFirstName.Text} {ctlLastName.Text}, I hope you're having a good day.";
+        }
+        */
     }
 }
